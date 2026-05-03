057a7440698ab49568ab07ad052c36c6b8bb4d17
AfriLove Team <afriloveteam@gmail.com>
2026-05-04 00:01:24 +0100

    refactor: collapse UI layers into single frontend source (remove legacy ui shell)

--- ui/partials/shared/state.js
+++
@@ -1,8 +0,0 @@
-export async function getState() {
-  try {
-    const res = await fetch('http://127.0.0.1:3000/afrid/state');
-    return await res.json();
-  } catch (e) {
-    return { mode: "USER", error: true };
-  }
-}
