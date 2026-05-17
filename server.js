a454ee08c9d8577fd65f16d50d160f4ce88520e8
AfriLove Team <afriloveteam@gmail.com>
2026-05-17 23:25:28 +0100

    deploy afridigital-api

--- server.js
+++ server.js
@@ -16,9 +16,8 @@ app.use('/whatsapp',whatsappGateway);
 const envCheck=require('./services/whatsapp-gateway/tools/envCheck');
 app.use('/whatsapp/tools',envCheck);
 
-const PORT=process.env.PORT||3000;
-app.listen(PORT,'0.0.0.0',()=>{
-  console.log('🚀 AFRIAI RUNNING ON PORT',PORT);
-});
-
 module.exports=app;
+app.listen(process.env.PORT || 3000, "0.0.0.0", () => {
+  console.log("🔥 SERVER RUNNING");
+  console.log("🚀 AFRIAI RUNNING ON PORT", process.env.PORT || 3000);
+});
