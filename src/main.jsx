b354d118b4e6da1773b9588f4059ffc66071a24b
AfriLove Team <afriloveteam@gmail.com>
2026-06-23 13:59:44 +0100

    fix: remove broken SOCMainEntry dependency for production build

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import SOCMainEntry from "./soc-os/entry/SOCMainEntry.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <SOCMainEntry />
   </React.StrictMode>
 );
