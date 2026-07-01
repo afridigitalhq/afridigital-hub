e647efcb1ad638c7ec1788cca45212197f5230c8
AfriLove Team <afriloveteam@gmail.com>
2026-07-01 06:09:28 +0100

    Landing page routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,6 @@
 import React from "react";
+import AppRouter from "./routes/AppRouter";
 
 export default function App() {
-  return (
-    <div style={{
-      background:"#000",
-      color:"#00ff88",
-      height:"100vh",
-      display:"flex",
-      justifyContent:"center",
-      alignItems:"center",
-      fontSize:"20px"
-    }}>
-      AFRIDIGITAL SAFE MODE ✔ FRONTEND IS ALIVE
-    </div>
-  );
+  return <AppRouter />;
 }
