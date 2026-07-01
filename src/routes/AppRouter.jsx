2333dd02e3834b3f12616c6a16a057290af71f38
AfriLove Team <afriloveteam@gmail.com>
2026-07-01 05:55:30 +0100

    VALIDATION: Restore Landing Page routing

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,56 +1,12 @@
-import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "../landing/LandingPage";
-import AuthPage from "../auth/AuthPage";
-import MainApp from "../app/MainApp";
-import AdminHome from "../adminhomepage/AdminHome";
-import GlobalNav from "../ui/GlobalNav";
-import AdminGuard from "../security/AdminGuard";
-import AfriAICommandDock from "../ai/AfriAICommandDock";
-import SystemStream from "../dashboard/SystemStream";
-
-function Layout({ children }) {
-  return (
-    <div style={{paddingBottom:60}}>
-      <GlobalNav />
-      {children}
-      <AfriAICommandDock />
-    </div>
-  );
-}
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/auth" element={<AuthPage />} />
-          <Route path="/app" element={<MainApp />} />
-
-          <Route path="/stream" element={<SystemStream />} />
-
-          <Route
-            path="/adminhomepage"
-            element={
-              <AdminGuard>
-                <AdminHome />
-              </AdminGuard>
-            }
-          />
-        
-        <Route path="/admin/control-room" element={<ControlRoomApp />} />
-      
-        <Route path="/admin/soc" element={<SOCSystem />} />
-      
-        <Route path="/admin/war-room" element={<WarRoom />} />
-      
-        <Route path="/admin/runtime" element={<RuntimeDashboard />} />
-      
-        <Route path="*" element={<LandingPage />} />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
       </Routes>
-    
-      </Layout>
     </BrowserRouter>
   );
 }
