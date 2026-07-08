6f5c08a4e82acceef47787ebe3beaa15298b808e
AfriLove Team <afriloveteam@gmail.com>
2026-07-08 18:21:34 +0100

    Mount AfriAI command dock in application shell

--- src/core/shell/AfriDigitalShell.jsx
+++ src/core/shell/AfriDigitalShell.jsx
@@ -1,8 +1,12 @@
-import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import useAfriAIRouter from "../afriai/useAfriAIRouter";
 import AfriBoostSidebar from "../../components/ads/AfriBoostSidebar";
+import AfriAICommandDock from "../../ai/AfriAICommandDock";
 
 export default function AfriDigitalShell({ children }) {
+
+  const location = useLocation();
+  const isLanding = location.pathname === "/";
 
   // activate ecosystem brain globally
   useAfriAIRouter();
@@ -10,6 +14,9 @@ export default function AfriDigitalShell({ children }) {
   return (
     <div>
       {children}
+
+      {/* GLOBAL AI COMMAND LAYER */}
+      {!isLanding && <AfriAICommandDock />}
 
       {/* GLOBAL MONETIZATION LAYER */}
       <AfriBoostSidebar />
