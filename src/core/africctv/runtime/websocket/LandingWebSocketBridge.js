bfaee387e1b2a00c7683b6684ad63fc073c9d78d
AfriLove Team <afriloveteam@gmail.com>
2026-07-09 23:21:42 +0100

    feat: connect AfriCCTV landing bridge to live runtime

--- src/core/africctv/runtime/websocket/LandingWebSocketBridge.js
+++ src/core/africctv/runtime/websocket/LandingWebSocketBridge.js
@@ -1,1 +1,26 @@
-export default class LandingWebSocketBridge {}
+import { afriCCTVLiveClient } from "../../../live/AfriCCTVLiveClient.js";
+
+export default class LandingWebSocketBridge {
+
+  constructor(){
+    this.listeners = new Set();
+  }
+
+  connect(){
+    afriCCTVLiveClient.subscribe((event)=>{
+      this.listeners.forEach(cb=>cb(event));
+    });
+
+    return afriCCTVLiveClient.connect();
+  }
+
+  subscribe(callback){
+    this.listeners.add(callback);
+  }
+
+  disconnect(){
+    this.listeners.clear();
+    afriCCTVLiveClient.disconnect();
+  }
+
+}
