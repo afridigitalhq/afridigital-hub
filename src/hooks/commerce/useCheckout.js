7829d3a51af7c1160d3b1fba869d3d2b0048a20e
AfriLove Team <afriloveteam@gmail.com>
2026-07-12 20:55:50 +0100

    feat(commerce): activate AfriCommerce application foundation

--- src/hooks/commerce/useCheckout.js
+++ src/hooks/commerce/useCheckout.js
@@ -1,10 +1,10 @@
 import AfriCommerceApiClient from "../../core/africommerce/api/AfriCommerceApiClient";
-import AfriCommerceEndpoints from "../../core/africommerce/api/AfriCommerceApiClient";
+import AfriCommerceEndpoints from "../../core/africommerce/api/AfriCommerceEndpoints";
 
 export default function useCheckout(){
 
   return AfriCommerceApiClient.request(
-    "/commerce/checkout"
+    AfriCommerceEndpoints.CHECKOUT
   );
 
 }
