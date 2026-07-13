533a5257a96c01a61599f0c4f6b9022e755a75c2
AfriLove Team <afriloveteam@gmail.com>
2026-07-13 18:42:52 +0100

    compose user dashboard home experience

--- src/pages/user/dashboard/UserDashboard.jsx
+++ src/pages/user/dashboard/UserDashboard.jsx
@@ -1,10 +1,24 @@
 import React from "react";
+import WelcomeBanner from "./partials/WelcomeBanner";
+import EcosystemOverview from "./partials/EcosystemOverview";
+import ProductLauncher from "./partials/ProductLauncher";
+import AfriAIPanel from "./partials/AfriAIPanel";
+import QuickActions from "./partials/QuickActions";
+import InsightsPanel from "./partials/InsightsPanel";
+import ActivityFeed from "./partials/ActivityFeed";
 import PluginWorkspace from "./partials/PluginWorkspace";
 
-export default function UserDashboard() {
+export default function UserDashboard(){
   return (
-    <>
+    <div style={{display:"grid",gap:16}}>
+      <WelcomeBanner />
+      <EcosystemOverview />
+      <ProductLauncher />
+      <AfriAIPanel />
+      <QuickActions />
+      <InsightsPanel />
+      <ActivityFeed />
       <PluginWorkspace />
-    </>
+    </div>
   );
 }
