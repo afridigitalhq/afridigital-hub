1060d355a3e51180bd19eacc20e7b5a66ad1f734
AfriLove Team <afriloveteam@gmail.com>
2026-07-27 00:53:32 +0100

    feat: add AfriTrust badge rendering runtime v2

---
+++ src/core/afritrust/rendering/AfriTrustBadgeDisplayOrder.js
@@ -0,0 +1,10 @@
+const AfriTrustBadgeDisplayOrder = [
+  "PLATFORM_TRUST",
+  "PRODUCT_TRUST",
+  "AFRIVERIFIED",
+  "AFRITICK_PREMIUM",
+  "PRODUCT_STATISTICS",
+  "PRODUCT_RATINGS"
+];
+
+export default AfriTrustBadgeDisplayOrder;
---
+++ src/core/afritrust/rendering/AfriTrustBadgeRenderer.js
@@ -0,0 +1,9 @@
+import AfriTrustBadgeDisplayOrder from "./AfriTrustBadgeDisplayOrder.js";
+
+const AfriTrustBadgeRenderer = {
+  getDisplayOrder(){
+    return AfriTrustBadgeDisplayOrder;
+  }
+};
+
+export default AfriTrustBadgeRenderer;
