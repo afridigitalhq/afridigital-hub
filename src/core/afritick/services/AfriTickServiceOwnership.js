be2879b3010a4905969320f458f1da698578b447
AfriLove Team <afriloveteam@gmail.com>
2026-07-27 01:09:40 +0100

    feat: add AfriTick backend service registry runtime v1

---
+++ src/core/afritick/services/AfriTickServiceOwnership.js
@@ -0,0 +1,15 @@
+const AfriTickServiceOwnership = {
+  owner:"AfriTickCore",
+  executionLayer:"AfriDigitalAPI",
+  consumers:[
+    "AfriCommerce",
+    "AfriWork",
+    "AfriAI",
+    "AfriDesignStudio",
+    "AfriEducation",
+    "AfriCCTV",
+    "AfriLogistics"
+  ]
+};
+
+export default AfriTickServiceOwnership;
---
+++ src/core/afritick/services/AfriTickServiceRegistry.js
@@ -0,0 +1,11 @@
+const AfriTickServiceRegistry = [
+  "MembershipService",
+  "BenefitsService",
+  "SubscriptionService",
+  "BillingService",
+  "PaymentService",
+  "VerificationService",
+  "NotificationService"
+];
+
+export default AfriTickServiceRegistry;
